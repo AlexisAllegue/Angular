ea8c563763b996ee9bbbf8fae89adace2792f42e
qallegue <qiuyi.allegue@gmail.com>
2024-08-16 09:53:46 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,10 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));});
+builder.Services.AddCors();
 var app = builder.Build();
 
 
-
+//Add middleware
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
